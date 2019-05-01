2f279b0bcbb5cc499f24ea0c6ccead31aef58223
Eduardo Demeneck Onghero <acrackintheice@gmail.com>
2019-05-01 16:51:59 -0300

    Adiciona novos componentes de interface e métodos para aquisição de dados da api do Github

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 function sillyAlert(){
     alert("I'm a silly test function")
 }
+
+async function fetchGithubUser(username){
+    const response = await fetch('https://api.github.com/users/' + username);
+    const user = await response.json();
+    console.log(JSON.stringify(user));
+    return user;
+}
+
+async function fetchGithubUserRepos(username){
+    const response = await fetch('https://api.github.com/users/' + username + '/repos');
+    const repos = await response.json();
+    console.log(JSON.stringify(repos));
+    return repos;
+}
+
+async function fetchGithubRepo(reponame){
+    const response = await fetch('https://api.github.com/repos/' + reponame);
+    const repo = await response.json();
+    console.log(JSON.stringify(repo));
+    return repo;
+}
